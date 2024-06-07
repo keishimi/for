0d66b810da1fb5b9d5dc17b3a1805386cf65d21a
keishimi <alex.safonovaa@yandex.ru>
2024-06-07 13:44:20 +0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,21 @@
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+
+for number in numbers:
+    is_prime=True
+    if number<=1:
+        is_prime=False
+    else:
+        for i in range(2,(number//2)+1):
+            if number%i==0:
+                is_prime=False
+                break
+    if is_prime:
+        primes.append(number)
+    else:
+        not_primes.append(number)
+
+print("Простые числа:", primes)
+print("Не простые числа:", not_primes)
